98ed1d8d9b2e14867100cd8c6b21f6113905e9cd
Bhushan Luitel <47515905+Bhushan2054@users.noreply.github.com>
2022-11-14 12:29:09 +0545

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+# Simple hello world using TensorFlow
+hello = tf.constant('Hello, TensorFlow!')
+
+# Start tf session
+sess = tf.Session()
+
+# Run the op
+print(sess.run(hello))
